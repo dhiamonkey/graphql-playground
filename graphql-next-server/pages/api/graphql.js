e634e571630ab87459cdba063f4ac034addd6b02
dhiamonkey <dhiamuhammad@gmail.com>
2022-04-15 17:13:39 +0700

    Server succesfully running, todo typeRefs resolvers

---
+++ graphql-next-server/pages/api/graphql.js
@@ -0,0 +1,36 @@
+import { ApolloServer, gql } from "apollo-server-micro";
+
+//TODO fix typeDefs and resolvers
+const typeDefs = gql`
+  type Query {
+    hello: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => {
+      return "hello";
+    },
+  },
+};
+
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const startServer = apolloServer.start();
+
+export default async function handler(req, res) {
+  await startServer;
+  await apolloServer.createHandler({
+    path: "/api/graphql",
+  })(req, res);
+}
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
